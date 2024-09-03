05fa1612bad911575ddd9080934742c55570ecd9
FabAlien <fabalien.von@gmail.com>
2024-09-03 11:05:04 +0200

    render out ghibli movie list from API

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ function burgerFunction() {
     x.className = 'navbar';
   }
 }
+
+// console.log("Starting Javascript")
+
+// while(true) {
+//   console.log("New Turn")
+//   await waitFor()
+// }
+
+// console.log("Done")
+
+// async function waitFor() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {resolve()}, 3000)
+//   })
+// }
+
+const apiUrl = "https://ghibliapi.vercel.app/films"
+
+const response = await fetch(apiUrl)
+const data = await response.json()
+
+const movieListElement = document.querySelector("#filmList")
+console.log(movieListElement)
+
+for (const movie of data) {
+  console.log(movie)
+
+  const movieCard = document.createElement("Li")
+  movieCard.textContent = movie.title
+
+  movieListElement.append(movieCard)
+}
+
+console.log(data)
